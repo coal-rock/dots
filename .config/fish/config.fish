f74e3b42278b984d63794a421a35c66041d05e56
coal-rock <colebrazinski15@gmail.com>
2026-02-03 21:07:32 -0500

    feat: update monitor layout

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -33,7 +33,7 @@ if status --is-interactive
 
     # scripts
     function goon 
-        xrandr --output eDP-1 --primary --mode 2560x1600 --pos 1080x1163 --rotate normal --auto --output DP-1 --mode 1920x1080 --pos 1080x83 --rotate normal --auto --output DP-2 --off --output DP-1-0.2 --mode 1920x1080 --pos 0x0 --rotate left --auto --output DP-1-0.3 --mode 1920x1080 --pos 3000x83 --rotate normal --auto --output DP-1-0 --off --output DP-1-1 --off --output HDMI-1-0 --mode 1440x900 --pos 3640x1163 --rotate normal --auto --output DP-1-2 --off --auto
+        xrandr --output eDP-1 --primary --mode 2560x1600 --pos 1080x1080 --rotate normal --output DP-1 --off --output DP-2 --off --output DP-1-0.2 --mode 1920x1080 --pos 0x126 --rotate left --output DP-1-0.3 --mode 1920x1080 --pos 2485x0 --rotate normal --output DP-1-0 --off --output DP-1-1 --off --output HDMI-1-0 --off --output DP-1-2 --off
     end
 
     function lap
